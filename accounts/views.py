06af15f42571d012c6a256fc507ab37fabdfc930
Ishita <2018ceb1008@iitrpr.ac.in>
2024-05-09 00:55:45 +0530

    Adding api for registration and minor restructuring of HTML

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def registerUser(request):
+    return HttpResponse("this is user registration form")
+
